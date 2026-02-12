c1385cff32cac470074ed62cad796937f44e4820
vardaa-v3 <sandeepdanger7841@gmail.com>
2026-02-12 22:44:32 +0530

    basics things

---
+++ first oops.py
@@ -0,0 +1,18 @@
+class employee:
+    def __init__(self):
+        self.id = 12
+        self.dep = "hr"
+
+    def city(self, area):
+        print(f"the person is based on this { area }")
+
+
+
+
+# creating the object of the class
+
+gojo = employee()
+
+gojo.city("banglore")
+
+
